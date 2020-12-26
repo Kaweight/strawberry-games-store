c367c6245cbf6f11bed9f3942125b2c394dff9b4
Kamil Mazelanik <kamilmaz9206@gmail.com>
2020-12-26 15:28:12 +0100

    some styling in ProductScreen

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -4,8 +4,43 @@ import { CssBaseline, Grid } from '@material-ui/core';
 import Header from '../components/Header';
 import { makeStyles } from '@material-ui/core/styles';
 import ProductSaleCard from '../components/ProductSaleCard';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { ShareOutlined, FavoriteBorderOutlined, AccountBalanceWalletOutlined, LanguageOutlined } from '@material-ui/icons';
 
 const useStyles = makeStyles({
+  root: {
+    minWidth: 200,
+    flexGrow: 1,
+    marginTop: '3rem',
+    backgroundColor: 'grey',
+  },
+  UpperIcons: {
+
+  },
+  BuyNow: {
+    marginBottom: 12,
+    backgroundColor: 'orange',
+    padding: 8,
+    borderRadius: '1.5rem',
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: '0.8rem',
+  },
+  PayPal: {
+    marginBottom: 12,
+    backgroundColor: '#74b9ff',
+    padding: 8,
+    borderRadius: '1.5rem',
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: '1rem',
+  },
   image: {
     maxWidth: 400,
     maxHeight: 400,
@@ -35,7 +70,26 @@ const ProductScreen = props => {
           <img className={classes.image} src={product.imageUrl} />
         </Grid>
         <Grid item xs={12} sm={2}>
-        <ProductSaleCard />
+        <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography className={classes.UpperIcons} variant="h5" component="h2">
+          <LanguageOutlined />
+          <ShareOutlined />
+        </Typography>
+        <Typography className={classes.BuyNow} gutterBottom>
+          BUY NOW!
+          {product.earlierPrice}
+          {product.price}
+        </Typography>
+        <Typography className={classes.PayPal} color="textSecondary">
+          KUP Z <i>PayPal</i>
+        </Typography>
+        <Typography variant="body2" component="p">
+          <FavoriteBorderOutlined />
+          <AccountBalanceWalletOutlined />
+        </Typography>
+      </CardContent>
+    </Card>
         </Grid>
         <Grid item xs={false} sm={2} />
       </Grid>
--- src/components/ProductSaleCard.js
+++ src/components/ProductSaleCard.js
@@ -1,55 +1,64 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+// import CardsInfo from '../static/CardsInfo';
+// import { ShareOutlined, FavoriteBorderOutlined, AccountBalanceWalletOutlined, LanguageOutlined } from '@material-ui/icons';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 200,
-    flexGrow: 1,
-    marginTop: '3rem',
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     maxWidth: 250,
+//     flexGrow: 1,
+//     marginTop: '3rem',
+//     backgroundColor: 'grey',
+//   },
+//   UpperIcons: {
 
-export default function OutlinedCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+//   },
+//   BuyNow: {
+//     fontSize: 14,
+//   },
+//   pos: {
+//     marginBottom: 12,
+//     backgroundColor: '#74b9ff',
+//     padding: 8,
+//     borderRadius: '1.5rem',
+//     textAlign: 'center',
+//     color: 'white',
+//     fontWeight: 'bold',
+//     fontSize: '1rem',
+//   },
+// });
 
-  return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
-}
+// const ProductSaleCard = props => {
+//   const classes = useStyles();
+//   // const product = CardsInfo.find(x => x._id === props.match.params.id);
+//   const { price, earlierPrice } = props;
+//   return (
+//     <Card className={classes.root} variant="outlined">
+//       <CardContent>
+//         <Typography className={classes.UpperIcons} variant="h5" component="h2">
+//           <LanguageOutlined />
+//           <ShareOutlined />
+//         </Typography>
+//         <Typography className={classes.BuyNow} color="textSecondary" gutterBottom>
+//           BUY NOW!
+//           {earlierPrice}
+//           {price}
+//         </Typography>
+//         <Typography className={classes.pos} color="textSecondary">
+//           KUP Z <i>PayPal</i>
+//         </Typography>
+//         <Typography variant="body2" component="p">
+//           <FavoriteBorderOutlined />
+//           <AccountBalanceWalletOutlined />
+//         </Typography>
+//       </CardContent>
+//     </Card>
+//   );
+// }
+
+// export default ProductSaleCard;
